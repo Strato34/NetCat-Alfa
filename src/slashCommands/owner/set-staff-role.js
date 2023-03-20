7b42dba0d1c93663018280c8413cc83e6e41b202
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2023-03-20 18:30:46 +0100

    created set-staff-role.js

---
+++ src/slashCommands/owner/set-staff-role.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    CMD: new SlashCommandBuilder()
+    .setName('set-staff-role')
+    .setDescription('Establece el rol del staff del servidor de NetCat')
+    .addRoleOption(option1 =>
+        option1.setName('rol')
+        .setDescription('Elige el rol del staff')
+        .setRequired(true)),
+
+        async execute(netcatcalfa, interaction) {
+            return interaction.reply("Comando en desarrollo.");
+        }
+}
