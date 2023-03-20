6c1b71a9aa1f39b68709c59d42d13211adf05933
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2023-03-20 19:58:47 +0100

    updated set-staff-role.js

--- src/slashCommands/owner/set-staff-role.js
+++ src/slashCommands/owner/set-staff-role.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const db = require('megadb');
 const staffroledb = new db.crearDB('staffroledb');
+const { ownerid } = require('../../core/socket.json');
 
 module.exports = {
     CMD: new SlashCommandBuilder()
@@ -12,6 +13,16 @@ module.exports = {
         .setRequired(true)),
 
         async execute(netcatcalfa, interaction) {
-            return interaction.reply("Comando en desarrollo.");
+            let rol = interaction.options.getRole('rol');
+            const staffrole = await staffroledb.obtener('staffrole');
+            if(!staffrole) {
+                staffroledb.establecer("staffrole", "ninguno");
+            }
+            if(interaction.user.id !== ownerid) {
+                interaction.reply({ content:`**:x: | PERMISO DENEGADO:** Solo <@${ownerid}> puede usar este comando!`, ephemeral: true}).catch(()=> { null; });
+            }
+            if(!rol) return interaction.reply({ content: "**:x: | ERROR:** El rol introducido no existe en este servidor.", ephemeral: true}).catch(()=> { null; });
+            staffroledb.establecer("staffrole", `${rol.id}`);
+            interaction.reply(`:white_check_mark: | Se ha establecido correctamente el rol ${rol} como rol de staff del servidor de NetCat.`).catch(()=> { null; });
         }
 }
