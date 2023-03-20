2e0129b52d36e37c0a7fd4306dc0b861f4ddec12
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2023-03-20 17:48:55 +0100

    updated ping.js

--- src/slashCommands/Info/ping.js
+++ src/slashCommands/Info/ping.js
@@ -1,17 +1,34 @@
-const {SlashCommandBuilder} = require('discord.js');
 
-module.exports = {
-    CMD: new SlashCommandBuilder()
-    .setDescription("Mira el ping del bot"),
+const { SlashCommandBuilder,
+    EmbedBuilder } = require('discord.js');
     
-    execute(Client, interaction, prefix, GUILD_DATA){
-        return interaction.reply(`\`${Client.ws.ping}ms\``)
+    module.exports = {
+        CMD: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Ping del bot'),
+    
+    async execute(netcatalfa, interaction) {
+        if(!interaction.channel.permissionsFor(netcatalfa.user).has("EmbedLinks")) return interaction.reply({  content: "**<a:netcatalfaemojiincorrecto:1080458827054465075> | ERROR:** No tengo los permisos suficientes. \nPermisos que me faltan: `EmbedLinks`", ephemeral: true }).catch(()=> { null; });
+        if(!interaction.channel.permissionsFor(netcatalfa.user).has("SendMessagesInThreads")) {
+            if(interaction.channel.type === 11) return interaction.reply({ content:"**<a:netcatalfaemojiincorrecto:1080458827054465075> | ERROR:** No tengo los permisos suficientes. \nPermisos que me faltan: `SendMessagesInThreads`", ephemeral: true}).catch(()=> { null; });
+            if(interaction.channel.type === 12) return interaction.reply({ content:"**<a:netcatalfaemojiincorrecto:1080458827054465075> | ERROR:** No tengo los permisos suficientes. \nPermisos que me faltan: `SendMessagesInThreads`", ephemeral: true}).catch(()=> { null; });
+        }
+        const time = Date.now()
+    
+        interaction.reply({ embeds: 
+            [new EmbedBuilder()
+            .setTitle("Midiendo latencia...")
+            .setDescription("Discord API: Calculando...\nTiempo de respuesta del bot: Calculando...")
+            .setColor('Yellow')]}).then(() => {
+            
+            let tiempoTotal = time - Date.now()
+            var resultado = Math.abs(tiempoTotal);
+            
+            interaction.editReply({ embeds: 
+                [new EmbedBuilder()
+                    .setTitle("Pong! 🏓")
+                    .setDescription("`Discord API:` " +netcatalfa.ws.ping+ " ms\n`Tiempo de respuesta del bot:` " +resultado+ " ms")
+                    .setColor('Green')]}).catch(()=> null);
+        })
+    },
     }
-}
-
-/*
-╔═════════════════════════════════════════════════════╗
-║    || - || Desarrollado por dewstouh#1088 || - ||   ║
-║    ----------| discord.gg/MBPsvcphGf |----------    ║
-╚═════════════════════════════════════════════════════╝
-*/
