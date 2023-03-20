cea8d6ba04739f43e4588b3c87589bb9166cb1cf
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2023-03-20 17:35:04 +0100

    mv ping.js ping.js.old

--- src/comandos/Info/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    DESCRIPTION: "Mira el ping del bot",
-    execute(client, message, args, prefix, GUILD_DATA){
-        return message.reply(`\`${client.ws.ping}ms\``);
-    }
-}
-
