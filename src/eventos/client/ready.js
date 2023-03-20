2c32bb8857f76fe02932235a76d816a0dac7e2cb
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2023-03-20 17:43:23 +0100

    updated ready.js

--- src/eventos/client/ready.js
+++ src/eventos/client/ready.js
@@ -1,8 +1,13 @@
+const { ActivityType } = require('discord.js');
 module.exports = async Client => {
     console.log(`Conectado como ${Client.user.tag}`.green);
     if(Client?.application?.commands) {
         Client.application.commands.set(Client.slashArray);
         console.log(`(/) ${Client.slashCommands.size} Comandos Publicados!`.green);
     }
+    var estado = "idle";
+    Client.user.setActivity({name: `a SamuelVM`, type: ActivityType.Listening});
+    Client.user.setStatus(`${estado}`);
+    console.log(`Bot: ${Client.user.username}\nEstado: ${estado}`);
+    
 }
-
